e623b4195b233cb3bf068532bec05210e9cef4a3
Maroof Qureshi <123865444+developermaroof@users.noreply.github.com>
2025-06-01 15:39:39 +0500

    Completed The Backend part

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/authController");
 const blogController = require("../controllers/blogController");
+const commentController = require("../controllers/commentController");
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
@@ -16,5 +17,8 @@ router.get("/blog/all", auth, blogController.getAll);
 router.get("/blog/:id", auth, blogController.getById);
 router.put("/blog/:id", auth, blogController.update);
 router.delete("/blog/:id", auth, blogController.delete);
+
+router.post("/comment", auth, commentController.create);
+router.get("/comment/:id", auth, commentController.getById);
 
 module.exports = router;
