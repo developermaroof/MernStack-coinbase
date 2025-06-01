2b907cf8d12ac579fcdffe9455b9729b64f425ac
Maroof Qureshi <123865444+developermaroof@users.noreply.github.com>
2025-06-01 13:05:00 +0500

    Added Blog Controller for GetAll and GetSingle

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const Blog = require("../models/blog");
 const { BACKEND_SERVER_PATH } = require("../config/index");
 const BlogDTO = require("../dto/blog");
+const BlogDetailsDTO = require("../dto/blog-details");
 
 const mongodbIdPattern = /^[0-9a-fA-F]{24}$/;
 
@@ -56,9 +57,49 @@ const blogController = {
     return res.status(201).json({ blog: blogDto });
   },
 
-  async getAll(req, res, next) {},
+  async getAll(req, res, next) {
+    try {
+      const blogs = await Blog.find({});
 
-  async getById(req, res, next) {},
+      const blogsDto = [];
+
+      for (let i = 0; i < blogs.length; i++) {
+        const dto = new BlogDTO(blogs[i]);
+
+        blogsDto.push(dto);
+      }
+
+      return res.status(200).json({ blogs: blogsDto });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async getById(req, res, next) {
+    const getByIdSchema = Joi.object({
+      id: Joi.string().regex(mongodbIdPattern).required(),
+    });
+
+    const { error } = getByIdSchema.validate(req.params);
+
+    if (error) {
+      return next(error);
+    }
+
+    let blog;
+
+    const { id } = req.params;
+
+    try {
+      blog = await Blog.findOne({ _id: id }).populate("author");
+    } catch (error) {
+      return next(error);
+    }
+
+    const blogDto = new BlogDetailsDTO(blog);
+
+    return res.status(200).json({ blog: blogDto });
+  },
 
   async update(req, res, next) {},
 
