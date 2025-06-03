3d0e65ccbeb93cb393fae8499d319dc53e0b93f3
Maroof Qureshi <123865444+developermaroof@users.noreply.github.com>
2025-06-03 22:13:34 +0500

    Added Login page Content and Validations with Formik and Yup

---
+++ frontend/src/components/TextInput/TextInput.jsx
@@ -0,0 +1,14 @@
+import styles from "./TextInput.module.css";
+
+const TextInput = (props) => {
+  return (
+    <div className={styles.textInputWrapper}>
+      <input {...props} />
+      {props.error && (
+        <p className={styles.errorMessage}>{props.errormessage}</p>
+      )}
+    </div>
+  );
+};
+
+export default TextInput;
--- frontend/src/pages/LogIn/LogIn.jsx
+++ frontend/src/pages/LogIn/LogIn.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import TextInput from "../../components/TextInput/TextInput";
+import styles from "./LogIn.module.css";
+import loginSchema from "../../schemas/loginSchema";
+import { useFormik } from "formik";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { values, touched, handleBlur, handleChange, errors } = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+
+    validationSchema: loginSchema,
+  });
+
+  return (
+    <div className={styles.loginWrapper}>
+      <div className={styles.loginHeader}>Log in to your account</div>
+      <TextInput
+        type="text"
+        value={values.username}
+        name="username"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        placeholder="username"
+        error={errors.username && touched.username ? 1 : undefined}
+        errormessage={errors.username}
+      />
+      <TextInput
+        type="password"
+        value={values.password}
+        name="password"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        placeholder="password"
+        error={errors.password && touched.password ? 1 : undefined}
+        errormessage={errors.password}
+      />
+      <button className={styles.logInButton}> Log In</button>
+      <span>
+        Don't have an account?{" "}
+        <button className={styles.createAccount}>Register</button>
+      </span>
+    </div>
+  );
 };
 
 export default Login;
